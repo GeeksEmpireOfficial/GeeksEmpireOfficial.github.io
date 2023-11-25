e9b22e1c12c173a866e61184f18d399be3a919d2
Elias Fazel <elias.fazel@gmail.com>
2023-11-25 06:07:42 -0800

    [Description]
    [ChangeLog]
    ~ Item
    
    [Build][ChangeLog]
    # Libraries Updated

--- geeksempire_website/lib/dashboard/sections/content/content.dart
+++ geeksempire_website/lib/dashboard/sections/content/content.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sachiel_website/dashboard/sections/content/provider/content_data_structure.dart';
+import 'package:sachiel_website/resources/colors_resources.dart';
 
 class Item extends StatefulWidget {
 
@@ -12,9 +13,16 @@ class Item extends StatefulWidget {
 }
 class _ItemState extends State<Item> {
 
+  Widget screenshotsPlaceholder = const SizedBox(
+    height: 537,
+  );
+
   @override
   void initState() {
     super.initState();
+
+    prepareScreenshots();
+
   }
 
   @override
@@ -41,10 +49,14 @@ class _ItemState extends State<Item> {
                  * Cover, Name, Summary
                  */
 
+                const SizedBox(
+                  height: 19,
+                ),
+
                 /* Start
                  * Screenshots
                  */
-
+                screenshotsPlaceholder
                 /* End
                  * Screenshots
                  */
@@ -74,23 +86,74 @@ class _ItemState extends State<Item> {
 
             ClipRRect(
               borderRadius: BorderRadius.circular(19),
-              child: Image.network(
-                widget.contentDataStructure.applicationCoverValue(),
+              child: SizedBox(
                 width: 549,
-                fit: BoxFit.cover,
-              ),
+                height: 267,
+                child: Image.network(
+                  widget.contentDataStructure.applicationCoverValue(),
+                  fit: BoxFit.cover,
+                ),
+              )
             ),
 
-            Row(
-              children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 37, right: 37, top: 19),
+              child: Row(
+                  children: [
 
-                Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(19),
-                  )
+                    Padding(
+                        padding: const EdgeInsets.only(),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(37),
+                            child: SizedBox(
+                                width: 137,
+                                height: 137,
+                                child: Image.network(
+                                  widget.contentDataStructure.applicationIconValue(),
+                                  fit: BoxFit.cover,
+                                )
+                            )
+                        )
+                    ),
+
+                    const SizedBox(
+                      width: 19,
+                    ),
+
+                    SizedBox(
+                        height: 137,
+                        width: 319,
+                        child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              widget.contentDataStructure.applicationNameValue(),
+                              style: const TextStyle(
+                                  color: ColorsResources.premiumLight,
+                                  fontSize: 41
+                              ),
+                            )
+                        )
+                    )
+
+                  ]
+              )
+            ),
+
+            Padding(
+                padding: const EdgeInsets.only(left: 19, right: 37, top: 19),
+                child: SizedBox(
+                    height: 73,
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          widget.contentDataStructure.applicationSummaryValue(),
+                          style: const TextStyle(
+                              color: ColorsResources.premiumLight,
+                              fontSize: 27
+                          ),
+                        )
+                    )
                 )
-
-              ]
             )
 
           ]
@@ -99,6 +162,59 @@ class _ItemState extends State<Item> {
   }
   /* End
    * Cover, Name, Summary
+   */
+
+  /* Start
+   * Screenshots
+   */
+  void prepareScreenshots() async {
+
+    List<Widget> allScreenshots = [];
+
+    widget.contentDataStructure.applicationScreenshotsValue().forEach((element) {
+
+      allScreenshots.add(screenshots(element));
+
+    });
+
+    setState(() {
+
+
+      screenshotsPlaceholder = SizedBox(
+        height: 537,
+        width: 1097,
+        child: Padding(
+            padding: const EdgeInsets.only(left: 37),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(37),
+                child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    children: allScreenshots
+                )
+            )
+        )
+      );
+
+    });
+
+  }
+
+  Widget screenshots(String screenshotLink) {
+
+    return Padding(
+      padding: const EdgeInsets.only(right: 13),
+      child: SizedBox(
+          height: 537,
+          width: 716,
+          child: Image.network(
+            screenshotLink,
+          )
+      )
+    );
+  }
+  /* End
+   * Screenshots
    */
 
 }
