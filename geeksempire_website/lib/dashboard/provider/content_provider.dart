0f8fc907549e808b0b752593a042fe878916143b
Elias Fazel <elias.fazel@gmail.com>
2023-12-05 05:56:40 -0800

    [Description]
    [ChangeLog]
    ~ Implementing Responsive Design for Mobile
    
    [Build][ChangeLog]
    # Libraries Updated

--- geeksempire_website/lib/dashboard/provider/content_provider.dart
+++ geeksempire_website/lib/dashboard/provider/content_provider.dart
@@ -16,7 +16,7 @@ class ContentProvider {
 
     allContent.add(ContentDataStructure('https://play.google.com/store/apps/details?id=net.geekstools.floatshort.PRO',
         'Floating Multitasking', 'The Fastest Floating Multitasking Application For Android',
-        'Floating Multitasking  We should be a Multitasking Master for today’s busy life. ⚡ While we cannot do anything about how fast the time flows, we still can control how we manage it. Being more productive helps us to live full capacity. Even when we work with several applications, there are many small actions that may affect our time management. Time is much more valuable for us to spend it in switching among apps!',
+        'Floating Multitasking. We should be a Multitasking Master for today’s busy life. ⚡ While we cannot do anything about how fast the time flows, we still can control how we manage it. Being more productive helps us to live full capacity. Even when we work with several applications, there are many small actions that may affect our time management. Time is much more valuable for us to spend it in switching among apps!',
         'https://play-lh.googleusercontent.com/FcYIaI1njwsDIRqvD6v2Uec27iGqUIuVcAqBWcdgrTGVayXlF0UVoqx97SibOAVfa8E=s512',
         'https://geeksempire.co/wp-content/uploads/2022/12/Float-It-Cover.png',
         'https://play-lh.googleusercontent.com/JC_-0jm7iun1SYVnH8MjSu9fTtIiiIbj26Y_dACc6Mp5cmcKdhjIzydagBrZ3vdvIo2F=w1080,https://play-lh.googleusercontent.com/dM2IwWS_WIRvLkyJt7P2xw6oUjPmYnXt33FJ_QyP2uKqXlZSQyTOZDup2Fp8toUmWd4=w1080,https://play-lh.googleusercontent.com/ZoFkeq-apLcOF4aVfBK3Uv9ttfQocPlsHS30u5sREm6gdQdzzatWQNqq3TPn4BG5o7o=w1080',
